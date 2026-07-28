b2687fd41ab40e3b9722f04ddd100ee6f84d7797
Mujeeb Cpy <mujeebcpy@gmail.com>
2026-07-28 19:59:30 +0530

    feat: add Solar Package DocType

---
+++ wahni_solar/wahni_solar_proposal/doctype/solar_package/test_solar_package.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, mujeebcpy and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSolarPackage(FrappeTestCase):
+	pass
